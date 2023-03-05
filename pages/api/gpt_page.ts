0e969ab953200515ac58163754f6b538b5207d76
Naren Manikandan <71474909+Naren219@users.noreply.github.com>
2023-03-05 13:12:53 -0500

    final before video

--- pages/api/gpt_page.ts
+++ pages/api/gpt_page.ts
@@ -13,47 +13,21 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const prompt = req.query.prompt;
-
+  console.log(prompt);
   if (!prompt) {
     return res.status(400).json({ error: "Prompt missing" });
   }
 
-//   if (prompt.length > 100) {
-//     return res.status(400).json({ error: "Prompt too long" });
-//   }
-
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `You are the best SAT test-taker and you need to answer questions and send their expert explanations. Forget all background knowledge of this passage:\n
-    Question and its context:\n
-    ${prompt}\n
-    Enter an Answer and Explanation:\n`,
-    max_tokens: 500,
-    temperature: 1,
-    presence_penalty: 0,
-    frequency_penalty: 0,
+    prompt: prompt as string,
+    max_tokens: 843,
+    temperature: 0.25,
+    presence_penalty: 0.9,
+    frequency_penalty: 1,
   });
 
-  const quote = completion.data.choices[0].text;
-  console.log(quote);
-  res.status(200).json({ quote });
+  const question = completion.data.choices[0].text;
+  console.log(question);
+  res.status(200).json({ quote: question });
 }
-
-/*
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-const response = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "You are an expert test-taker about to take the SAT. You will consider how the passage might be edited to correct errors in sentence structure, usage, or punctuation. \n\npassage 1\nAlso, because it is more concentrated, Greek yogurt contains slightly more protein per serving, thereby helping people stay satiated for longer periods of time.\n\nchange all instances of the word \"satiated\" with a synonym from the below choices that best maintains the flow of passage 2. Pay attention to the purpose of the passage and which synonym matches up most with it. Choose the answer to each question that most effectively improves the quality of writing in the passage or that makes the passage conform to the conventions of standard written English. If the word \"satiated\" is the best word in context, select \"A\". Output both a letter choice and an explanation of why the other choices are incorrect: \n\nA) satiated\nB) fulfilled\nC) complacent\nD) sufficient\n\nA) satiated - Satiated is the best choice because it conveys the idea that people are feeling full and satisfied after eating Greek yogurt, which is what the passage is trying to communicate. Fulfilled implies a sense of satisfaction from achieving something, complacent suggests contentment without effort or interest, and sufficient implies having enough but not necessarily being satisfied.",
-  temperature: 0.25,
-  max_tokens: 843,
-  top_p: 0.5,
-  frequency_penalty: 1,
-  presence_penalty: 0.9,
-});
-*/
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,15 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import mainImage from '@/assets/images/sat_image.jpeg'
-import { Form, Button, Spinner, ButtonGroup, FormLabel, ToggleButton, ToggleButtonGroup } from 'react-bootstrap'
+import { Form, Button, Spinner, FormLabel, ToggleButton, ToggleButtonGroup } from 'react-bootstrap'
 import { FormEvent, useState, SetStateAction } from 'react'
 import ConvertApi from 'convertapi-js'
 
 export default function Home() {
 
-  const [quote, setQuote] = useState("");
-  const [quoteLoading, setQuoteLoading] = useState(false);
-  const [quoteLoadingError, setQuoteLoadingError] = useState(false);
+  const [question, setQuestion] = useState("");
+  const [questionLoading, setQuestionLoading] = useState(false);
+  const [questionLoadingError, setQuestionLoadingError] = useState(false);
 
   const [urlData, setUrlData] = useState("");
   const [urlLoading, seturlLoading] = useState(false);
@@ -23,16 +23,27 @@ export default function Home() {
 
   const [userText, setUserText] = useState("Edit your passages here. Beware of text changes (these free pdf readers aren't always the best...)");
 
+  const [writingPrompt1, setWritingPrompt1] = useState(false);
+  const [writingPrompt2, setWritingPrompt2] = useState(false);
+
   const handleTextChange = (event: { target: { value: SetStateAction<string> } }) => {
     setUrlData(event.target.value);
   };
 
+  function handleWritingPrompt1() {
+    setWritingPrompt1(!writingPrompt1);
+  }
+
+  function handleWritingPrompt2() {
+    setWritingPrompt2(!writingPrompt2);
+  }
+
   function handleWritingChange() {
     setWritingQuestion(!writingQuestion);
     if (writingQuestion) {
-      setUserText("Input the passage here and your question in the next text box");
+      setUserText("Input the passage here (and remove all the whitespace after importing it through the pdf) and give plenty of context for your question. Utilize Ctrl+F to find the question");
     } else {
-      setUserText("Edit your passages here. Beware of text changes (these free pdf readers aren't always the best...)");
+      setUserText("Edit your passage here. Beware of text changes (these free pdf readers aren't always the best...) and give plenty of context to your question");
     }
     console.log(writingQuestion);
   }
@@ -68,7 +79,6 @@ export default function Home() {
         } finally {
           seturlRequestLoading(false);
         }
-        
       } catch (error) {
         console.error(error);
         seturlLoadingError(true);
@@ -82,25 +92,38 @@ export default function Home() {
   async function getGPTResponse(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
-    const prompt = formData.get("prompt")?.toString().trim();
-
-    console.log(prompt);
+    let prompt = formData.get("prompt")?.toString().trim();
+    let replace_word = formData.get("replace_word")?.toString().trim();
+    let other_choices = formData.get("other_choices")?.toString().trim();
+    replace_word = `${replace_word}`;
 
     if (prompt) {
       try {
-        setQuote("");
-        setQuoteLoadingError(false);
-        setQuoteLoading(true);
-        
+        setQuestion("");
+        setQuestionLoadingError(false);
+        setQuestionLoading(true);
+        if (!writingQuestion) {
+          let beg = `You are an expert test-taker about to take the SAT. You will consider how the passage might be edited to correct errors in sentence structure, usage, or punctuation. Choose the answer to each question that most effectively improves the quality of writing in the passage or that makes the passage conform to the conventions of standard written English.\n\nPassage 1\n${prompt}\n`
+          if (writingPrompt1) {
+            prompt = beg + `Change all instances of the word ${replace_word} with a synonym from the below choices that best maintains the flow of passage 1. Pay attention to the purpose of the passage and which synonym matches up most with it. If the word ${replace_word} is the best word in context, select "A". Output both a letter choice and an explanation of why:\nA) ${replace_word} \n${other_choices}\n`
+          } else if (writingPrompt2) {
+            prompt = beg + `Choose an answer choice below (a, b, c, d) that best serves as a replacement to the word ${replace_word} in the passage and write the explanation for your chosen answer with and why the other choices are not right with specific english grammar reasoning using this format "choice [insert choice letter here] is not the right answer because ..."\nA) ${replace_word} \n${other_choices}\n`
+          }
+        } else {
+          prompt = `You are the best SAT test-taker and you need to answer questions and send their expert explanations. Forget all background knowledge of this passage:\n
+          Question and its passage:\n
+          ${prompt}\n
+          Enter an Answer and Explanation:\n`
+        }
         const response = await fetch("/api/gpt_page?prompt=" + encodeURIComponent(prompt));
         const body = await response.json();
-
-        setQuote(body.quote);
+        
+        setQuestion(body.question);
       } catch (error) {
         console.error(error);
-        setQuoteLoadingError(true);
+        setQuestionLoadingError(true);
       } finally {
-        setQuoteLoading(false);
+        setQuestionLoading(false);
       }
     }
   }
@@ -143,16 +166,17 @@ export default function Home() {
         <FormLabel>You have selected the {writingQuestion ? "reading" : "writing"} type</FormLabel>      
         <FormLabel>If you want to directly input your text, paste it into the <b>last </b> text box below. </FormLabel>      
         <br />
+        {!writingQuestion && <FormLabel>Notice that software works well mainly on question that ask for the best replacement of another word already in the passage and replacing small grammatially-incorrect phrases. </FormLabel>}
 
         <Form onSubmit={fetchData} className={styles.inputForm}>
           <Form.Group className={styles.highlight} controlId='url-input'>
-            <Form.Label>Or type the URL of your PDF</Form.Label>
+            <Form.Label>Or type the URL of your PDF and it will appear below</Form.Label>
             <Form.Control
               name='url'
               placeholder='e.g. https://www.example.com/my-pdf.pdf'
             />
           </Form.Group>
-          <Button type='submit' className={styles.custom_btn} disabled={quoteLoading}>
+          <Button type='submit' className={styles.custom_btn} disabled={questionLoading}>
             Get data
           </Button>
         </Form>
@@ -168,7 +192,7 @@ export default function Home() {
               name='prompt'
               placeholder='e.g. Akira came directly, breaking all tradition...'
               as="textarea"
-              rows={8}
+              rows={10}
               value={urlData}
               onChange={handleTextChange}
             />
@@ -178,25 +202,46 @@ export default function Home() {
 
           {!writingQuestion ? (
             <Form.Group className='mb-3' controlId='passage-input'>
-              <Form.Label>Input the question here</Form.Label>
+              <Form.Label>Enter the word that needs to be replaced (aka, the word kept by "NO CHANGE")</Form.Label>
               <Form.Control
-                name='question'
+                name='replace_word'
+              />
+              <br />
+              <Form.Label>Enter the rest of the choices</Form.Label>
+              <Form.Control
+                name='other_choices'
                 as='textarea'
                 rows={3}
-                placeholder='Which choice best describes...'
+                placeholder={`B) However,\nC) As such,\nD) Moreover,`}
               />
+              <br />
+              <Form>
+                <Form.Check
+                  type="radio"
+                  label="If your question is asking to replace an existing word using context, select this option."
+                  id="radio1"
+                  name="writing-prompt"
+                  onChange={handleWritingPrompt1}
+                />
+                <Form.Check
+                  type="radio"
+                  label="If there is a grammatical error that you need to correct with a phrase, select this option."
+                  id="radio2"
+                  name="writing-prompt"
+                  onChange={handleWritingPrompt2}
+                />
+              </Form>
             </Form.Group>
           ) : null}
           
-          <br />
-
-          <Button type='submit' className={styles.custom_btn} disabled={quoteLoading}>
+          <Button type='submit' className={styles.custom_btn} disabled={questionLoading}>
             Submit to model
           </Button>
         </Form>
-        {quoteLoading && <Spinner animation='border' />}
-        {quoteLoadingError && "Something went wrong. Please try again."}
-        {quote && <h5>{quote}</h5>}
+        {questionLoading && <Spinner animation='border' />}
+        {questionLoadingError && "Something went wrong. Please try again."}
+        <br />
+        {question && <p>{question}</p>}
       </main>
     </>
   )
