12b65e588451fd8f476cb0e85802dd11b72361ea
Naren Manikandan <71474909+Naren219@users.noreply.github.com>
2023-03-05 10:50:31 -0500

    completed base app without writing questions

--- pages/api/gpt_page.ts
+++ pages/api/gpt_page.ts
@@ -34,8 +34,26 @@ export default async function handler(
     frequency_penalty: 0,
   });
 
-  const question = completion.data.choices[0].text;
-
-  console.log(question);
-  res.status(200).json({ quote: question });
+  const quote = completion.data.choices[0].text;
+  console.log(quote);
+  res.status(200).json({ quote });
 }
+
+/*
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "You are an expert test-taker about to take the SAT. You will consider how the passage might be edited to correct errors in sentence structure, usage, or punctuation. \n\npassage 1\nAlso, because it is more concentrated, Greek yogurt contains slightly more protein per serving, thereby helping people stay satiated for longer periods of time.\n\nchange all instances of the word \"satiated\" with a synonym from the below choices that best maintains the flow of passage 2. Pay attention to the purpose of the passage and which synonym matches up most with it. Choose the answer to each question that most effectively improves the quality of writing in the passage or that makes the passage conform to the conventions of standard written English. If the word \"satiated\" is the best word in context, select \"A\". Output both a letter choice and an explanation of why the other choices are incorrect: \n\nA) satiated\nB) fulfilled\nC) complacent\nD) sufficient\n\nA) satiated - Satiated is the best choice because it conveys the idea that people are feeling full and satisfied after eating Greek yogurt, which is what the passage is trying to communicate. Fulfilled implies a sense of satisfaction from achieving something, complacent suggests contentment without effort or interest, and sufficient implies having enough but not necessarily being satisfied.",
+  temperature: 0.25,
+  max_tokens: 843,
+  top_p: 0.5,
+  frequency_penalty: 1,
+  presence_penalty: 0.9,
+});
+*/
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,40 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import mainImage from '@/assets/images/sat_image.jpeg'
-import { Form, Button, Spinner } from 'react-bootstrap'
-import { FormEvent, useState, useEffect, SetStateAction } from 'react'
+import { Form, Button, Spinner, ButtonGroup, FormLabel, ToggleButton, ToggleButtonGroup } from 'react-bootstrap'
+import { FormEvent, useState, SetStateAction } from 'react'
 import ConvertApi from 'convertapi-js'
 
 export default function Home() {
 
-  const [question, setQuestion] = useState("");
-  const [questionLoading, setQuestionLoading] = useState(false);
-  const [questionLoadingError, setQuestionLoadingError] = useState(false);
+  const [quote, setQuote] = useState("");
+  const [quoteLoading, setQuoteLoading] = useState(false);
+  const [quoteLoadingError, setQuoteLoadingError] = useState(false);
 
-  const [url, setUrl] = useState("");
+  const [urlData, setUrlData] = useState("");
   const [urlLoading, seturlLoading] = useState(false);
   const [urlLoadingError, seturlLoadingError] = useState(false);
   
-  const [urlData, setUrlData] = useState("");
+  const [urlRequestLoading, seturlRequestLoading] = useState(false);
+  const [urlRequestLoadingError, seturlRequestLoadingError] = useState(false);
+
+  const [writingQuestion, setWritingQuestion] = useState(false);
+
+  const [userText, setUserText] = useState("Edit your passages here. Beware of text changes (these free pdf readers aren't always the best...)");
 
   const handleTextChange = (event: { target: { value: SetStateAction<string> } }) => {
     setUrlData(event.target.value);
   };
+
+  function handleWritingChange() {
+    setWritingQuestion(!writingQuestion);
+    if (writingQuestion) {
+      setUserText("Input the passage here and your question in the next text box");
+    } else {
+      setUserText("Edit your passages here. Beware of text changes (these free pdf readers aren't always the best...)");
+    }
+    console.log(writingQuestion);
+  }
 
   async function fetchData(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
@@ -29,48 +44,63 @@ export default function Home() {
 
     if (url) {
       try {
-        setUrl("");
+        setUrlData("");
         seturlLoadingError(false);
         seturlLoading(true);
 
         let convertApi = ConvertApi.auth('aCtka8VCDN5R6U3y')
-        let params = convertApi.createParams()
-        params.add('File', new URL(url));
-        let result = await convertApi.convert('pdf', 'txt', params)
-        let url_txt = result.files[0].Url
-    
-        const response = await fetch(url_txt);
-        const data = await response.text();
-        setUrlData(data);
+        try {
+          seturlRequestLoadingError(false);
+          seturlRequestLoading(true);
+
+          let params = convertApi.createParams()
+          params.add('File', new URL(url));
+          let result = await convertApi.convert('pdf', 'txt', params)
+          let url_txt = result.files[0].Url
+      
+          const response = await fetch(url_txt);
+          const data = await response.text();
+
+          setUrlData(data.trim());
+        } catch (error) {
+          console.error(error);
+          seturlRequestLoadingError(true);
+        } finally {
+          seturlRequestLoading(false);
+        }
+        
       } catch (error) {
         console.error(error);
-        setQuestionLoadingError(true);
+        seturlLoadingError(true);
       } finally {
-        setQuestionLoading(false);
+        seturlLoading(false);
       }
     }
   
   }
 
-  async function getGPTResponse(e: FormEvent<HTMLFormElement>, prompt_type: string) {
+  async function getGPTResponse(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
-    const prompt = formData.get(prompt_type)?.toString().trim();
+    const prompt = formData.get("prompt")?.toString().trim();
+
+    console.log(prompt);
+
     if (prompt) {
       try {
-        setQuestion("");
-        setQuestionLoadingError(false);
-        setQuestionLoading(true);
-
-        const response = await fetch("/api/cringe?prompt=" + encodeURIComponent(prompt));
+        setQuote("");
+        setQuoteLoadingError(false);
+        setQuoteLoading(true);
+        
+        const response = await fetch("/api/gpt_page?prompt=" + encodeURIComponent(prompt));
         const body = await response.json();
-        console.log(body);
-        setQuestion(body.question);
+
+        setQuote(body.quote);
       } catch (error) {
         console.error(error);
-        setQuestionLoadingError(true);
+        setQuoteLoadingError(true);
       } finally {
-        setQuestionLoading(false);
+        setQuoteLoading(false);
       }
     }
   }
@@ -84,59 +114,56 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <div className={styles.highlight}>
+        <br />
         <h1>SAT GPT</h1>
         <h2>powered by GPT-3</h2>
         <br />
         <div><h5>Enter a SAT reading question and with its passage and get instant answers</h5></div>
         <br /> 
         <br />
+        </div>
         <div className={styles.mainImageContainer}>
           <Image
             src={mainImage}
-            // fill
+            fill
             alt='main image'
             priority
             className={styles.mainImage}
           />
         </div>
+
         <br />
-        <Form onSubmit={(e) => getGPTResponse(e, "prompt_direct")} className={styles.inputForm}>
-          <Form.Group className='mb-3' controlId='prompt-input'>
-            <Form.Label>Enter your passage and question here</Form.Label>
-            <Form.Control
-              name='prompt'
-              placeholder='e.g. Akira came directly, breaking all tradition...'
-              as="textarea"
-              rows={8}
-              // maxLength={100}
-            />
-          </Form.Group>
-          <Button type='submit' className='mb-3' disabled={questionLoading}>
-            Get Responses
-          </Button>
-        </Form>
-        {questionLoading && <Spinner animation='border' />}
-        {questionLoadingError && "Something went wrong. Please try again."}
-        {question && <h5>{question}</h5>}
+        <FormLabel>Pick your question type</FormLabel>
+        <ToggleButtonGroup type="checkbox" aria-label="type">
+          <ToggleButton className={styles.custom_btn} variant="secondary" value={1} onClick={!writingQuestion ? handleWritingChange : undefined}>Reading</ToggleButton>
+          <ToggleButton className={styles.custom_btn} variant="secondary" value={2} onClick={writingQuestion ? handleWritingChange : undefined}>Writing</ToggleButton>
+        </ToggleButtonGroup>
         <br />
+        <FormLabel>You have selected the {writingQuestion ? "reading" : "writing"} type</FormLabel>      
+        <FormLabel>If you want to directly input your text, paste it into the <b>last </b> text box below. </FormLabel>      
+        <br />
+
         <Form onSubmit={fetchData} className={styles.inputForm}>
-          <Form.Group className='mb-3' controlId='url-input'>
+          <Form.Group className={styles.highlight} controlId='url-input'>
             <Form.Label>Or type the URL of your PDF</Form.Label>
             <Form.Control
               name='url'
               placeholder='e.g. https://www.example.com/my-pdf.pdf'
             />
           </Form.Group>
-          <Button type='submit' className='mb-3' disabled={questionLoading}>
+          <Button type='submit' className={styles.custom_btn} disabled={quoteLoading}>
             Get data
           </Button>
         </Form>
-        {questionLoading && <Spinner animation='border' />}
-        {questionLoadingError && "Something went wrong. Please try again."}
+        {urlRequestLoading && <Spinner animation='border' />}
+        {urlRequestLoadingError && "Something went wrong. Please try again."}
+        
         <br />
-        <Form onSubmit={(e) => getGPTResponse(e, "prompt_url")} className={styles.inputForm}>
+
+        <Form onSubmit={getGPTResponse} className={styles.inputForm}>
           <Form.Group className='mb-3' controlId='prompt-input'>
-            <Form.Label>Edit your passages here. <b>Beware of text changes </b>(these free pdf readers aren't always the best...)</Form.Label>
+            <Form.Label>{userText}</Form.Label>
             <Form.Control
               name='prompt'
               placeholder='e.g. Akira came directly, breaking all tradition...'
@@ -144,16 +171,32 @@ export default function Home() {
               rows={8}
               value={urlData}
               onChange={handleTextChange}
-              // maxLength={100}
             />
           </Form.Group>
-          <Button type='submit' className='mb-3' disabled={questionLoading}>
+
+          <br />
+
+          {!writingQuestion ? (
+            <Form.Group className='mb-3' controlId='passage-input'>
+              <Form.Label>Input the question here</Form.Label>
+              <Form.Control
+                name='question'
+                as='textarea'
+                rows={3}
+                placeholder='Which choice best describes...'
+              />
+            </Form.Group>
+          ) : null}
+          
+          <br />
+
+          <Button type='submit' className={styles.custom_btn} disabled={quoteLoading}>
             Submit to model
           </Button>
         </Form>
-        {questionLoading && <Spinner animation='border' />}
-        {questionLoadingError && "Something went wrong. Please try again."}
-        {question && <h5>{question}</h5>}
+        {quoteLoading && <Spinner animation='border' />}
+        {quoteLoadingError && "Something went wrong. Please try again."}
+        {quote && <h5>{quote}</h5>}
       </main>
     </>
   )
